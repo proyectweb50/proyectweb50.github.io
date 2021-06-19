c3c2ef1f983a36145ee19f7c628f32de1603faa5
proyectweb50 <85804457+proyectweb50@users.noreply.github.com>
2021-06-19 12:27:27 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyCEigu6rQK0hz93A9w1eDsJEuqtBUZvxpI",
-    authDomain: "thepill-b7b71.firebaseapp.com",
-    projectId: "thepill-b7b71",
-    storageBucket: "thepill-b7b71.appspot.com",
-    messagingSenderId: "1037443010193",
-    appId: "1:1037443010193:web:a97c9ec63017c684a57930",
-    measurementId: "G-SCH0FGWE2L"
+apiKey: "AIzaSyBx8Y6-uRi_eqnUxYvXWBl2AXpykdrFu2c",
+    authDomain: "proyectweb50.firebaseapp.com",
+    projectId: "proyectweb50",
+    storageBucket: "proyectweb50.appspot.com",
+    messagingSenderId: "865686280976",
+    appId: "1:865686280976:web:25f0ddc8e563fc48386395",
+    measurementId: "G-3Z1TZVZMW9"
 });

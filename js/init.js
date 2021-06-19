b2d3f4423c02e98e8d5929c1714d330234d3376e
proyectweb50 <85804457+proyectweb50@users.noreply.github.com>
2021-06-19 05:02:29 -0500

    Update and rename autentificacion.js to init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyAbg76VGicrgKCtxvndlk2e6ydX_XVJrFY",
+ apiKey: "AIzaSyAbg76VGicrgKCtxvndlk2e6ydX_XVJrFY",
     authDomain: "thepill-7caff.firebaseapp.com",
     projectId: "thepill-7caff",
     storageBucket: "thepill-7caff.appspot.com",
     messagingSenderId: "418863132178",
     appId: "1:418863132178:web:5340d8b9e4a01ed33df2cb",
     measurementId: "G-5FYSYL7WFE"
-
 });
